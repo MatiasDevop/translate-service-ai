6990fec180c4784bdc06e52efd54eb516e5dda22
Matt <solomatias321@gmail.com>
2024-10-03 21:43:14 +0200

    create api to access index.html

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPexception, Request, Depends 
+from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+app = FastAPI()
+
+#Setup for Jinja2 templates
+BASE_DIR = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+@app.get('/index', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html",  { "request": request})
